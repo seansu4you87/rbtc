304e902c0c78b21bae985ab925268f6814300918
Sean Yu <sean.yu@coinbase.com>
2018-09-20 23:49:19 -0700

    EventMachine works!

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ source "https://rubygems.org"
 ruby '~> 2.5.1'
 
 gem "bitcoin-ruby"
+gem "eventmachine" # TODO(yu): kill this later?
 
---
+++ lib/em.rb
@@ -0,0 +1,92 @@
+require 'eventmachine'
+require 'bitcoin/connection'
+
+Bitcoin.network = ARGV[0] || :bitcoin
+# Bitcoin.network = :testnet
+
+class RawJSON_Connection < Bitcoin::Connection
+  def on_handshake_begin
+    puts "handshake begin"
+    version = Bitcoin::Protocol::Version.new(
+        last_block: 127953,
+        from: "127.0.0.1:8333",
+        to: @sockaddr.reverse.join(":"),
+        user_agent: "/rbtc:0.0.1/",
+        relay: true
+    )
+    puts "-> version: #{version.fields}"
+    send_data(version.to_pkt)
+  end
+
+  def on_version(version)
+    puts "<- version: #{version.fields}"
+  end
+
+  def on_verack
+    puts "<- verack"
+    on_handshake_complete
+  end
+
+  def on_handshake_complete
+    puts "handshake complete with #{@sockaddr}"
+    @connected = true
+
+    query_blocks
+  end
+
+  def send_data(data)
+    puts "-> data: #{data}"
+    super(data)
+  end
+
+  def receive_data(data)
+    puts "<- data: #{data}"
+    super(data)
+  end
+
+  def on_ping(nonce)
+    puts "<- ping: #{nonce}"
+    if nonce
+      pong = Bitcoin::Protocol.pong_pkt(nonce)
+      puts "-> pong: #{pong}"
+      send_data(pong)
+    end
+  end
+
+  def on_addr(addr)
+
+  end
+
+  def on_inv_block(hash)
+
+  end
+
+  def on_inv_transaction(hash)
+
+  end
+
+  def on_block(block)
+    puts "block <- peer: #{block.hash}"
+  end
+
+  def on_tx(tx)
+    puts "tx <- peer: #{tx.hash}"
+  end
+
+  def on_get_block(hash)
+
+  end
+
+  def on_get_transaction(hash)
+
+  end
+end
+
+EM.run do
+  connections = []
+  # host = '127.0.0.1'
+  # host = '217.157.1.202'
+
+  # RawJSON_Connection.connect(host, 8333, connections)
+  RawJSON_Connection.connect_random_from_dns(connections)
+end
